7350f55f1e18c877391e0f0e4256f7e6f4b32ec5
sm6r <101045970+SajaMatar@users.noreply.github.com>
2024-06-23 14:36:31 +0300

    added 2fa

--- Login_Registration_Project/src/includes/login.inc.php
+++ Login_Registration_Project/src/includes/login.inc.php
@@ -1,21 +1,20 @@
 <?php
-session_start();
 require_once "./sessionMang.inc.php"; 
 
 if ($_SERVER['REQUEST_METHOD'] === "POST"&& isset($_SESSION['LoginNo'])) {
+    
     $recaptcha = $_POST['g-recaptcha-response'];
-    $secret_key='6LequagpAAAAAD9jbtINyMkDJDHGh2w9IA7XC_00';
+    $secret_key='####';
     $url = 'https://www.google.com/recaptcha/api/siteverify?secret='. $secret_key . '&response=' . $recaptcha; 
     $response = file_get_contents($url); 
     $response = json_decode($response); 
     if ($response->success != true){
-       $_SESSION['errors']['invaali reCaptcha'] = "Check the reCaptcha";
+       $_SESSION['errors'] ['invaali reCaptcha'] = "Check the reCaptcha";
         header("Location: ../index.php");
         die();
     } 
 
     $_SESSION['LoginNo']+=1;
-    HandleID();
     require_once "./DBconn.inc.php";
     require_once "./loginMVC/loginM.inc.php";
     require_once "./loginMVC/loginC.inc.php";
@@ -38,7 +37,16 @@ if ($_SERVER['REQUEST_METHOD'] === "POST"&& isset($_SESSION['LoginNo'])) {
     if (checkCreds($PDO, $username, $passwd)) {
         $_SESSION['authed'] = "1";
         $_SESSION['usr']=$username;
-        header("Location: ./welcome.inc.php");
+
+        if (is2FAen($PDO,$username)){
+            header("Location: ../2faCode.php");
+        }
+    
+        else{
+            header("Location: ./welcome.inc.php");
+            die();   
+        }
+
         die();
     } 
     else {
@@ -48,10 +56,9 @@ if ($_SERVER['REQUEST_METHOD'] === "POST"&& isset($_SESSION['LoginNo'])) {
         die();
     }
     
+    
 }
 else {
     header("Location: ../index.php");
     die();
 }
-
-
