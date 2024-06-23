2ac59db79686468c16f681650f7a89e8685849e7
sm6r <101045970+SajaMatar@users.noreply.github.com>
2024-06-23 14:33:56 +0300

    new password policy update

--- Login_Registration_Project/src/includes/newPass.inc.php
+++ Login_Registration_Project/src/includes/newPass.inc.php
@@ -1,23 +1,32 @@
 <?php 
 require_once "./sessionMang.inc.php";
 
-if($_SERVER['REQUEST_METHOD']==='POST' && isset($_SESSION['beenInNewPass']) && isset($_SESSION['AuthedForNewPass'])){
+if($_SERVER['REQUEST_METHOD']==='POST' && isset($_SESSION['beenInNewPass']) && isset($_SESSION['AuthedForNewPass']) ){
     if($_POST['new_password'] === $_POST['confirm_password']){
         require_once "./DBconn.inc.php";
         require_once "./passResetMVC/passResetM.inc.php";
         require_once "./passResetMVC/passResetC.inc.php";
-       
+        require_once "./signupMVC/singupM.inc.php";
+
+        if(badPassComp($_POST['new_password'])){
+            $_SESSION['errors']['badComp']='Password should be at least 8 characters in length, should include at least one upper case letter, one number and one special character.';
+            header("Location: ../NewPass.php");
+            die();
+        }
+        else{    
         updatePass($PDO,$_POST['new_password']);
         deleteToken($PDO,$_SESSION['AuthedForNewPass']);
+        $_SESSION['Subs']=0;
         $_SESSION['UpdatedSucc']='Password Updated Successfully try login  .... ';
         header("Location: ../index.php");
         die();
-
+        }
 
    }
    else{
    $_SESSION['errors']['PassesDontMatch']='The two passwords dont match ...';
    header("Location: ../NewPass.php");
+   die();
 }
 
 }
